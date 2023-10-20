6443414aca00e8396b3323fc62b928219c9fad07
ANGELMORENO23 <chinoman309@hotmail.com>
2023-10-19 23:09:28 -0300

    mercado libre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+
+
+app.use(express.static("public"));
+
+app.listen(8000,()=>{
+    console.log("servicio ubicado en http://localhost:8000/");
+})

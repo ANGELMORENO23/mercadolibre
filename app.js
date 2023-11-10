268fe86bfda4fad8c427cee46deecb6f45f79798
ANGELMORENO23 <chinoman309@hotmail.com>
2023-11-10 01:42:23 -0300

    agrege repo formularios, creacion de ingresar y registrase

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ const path = require("path")
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
-
+app.get("/ofertas",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/ofertas.html"));
+});
+app.get("/tiendas",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/tiendas.html"));
+});
+app.get("/vender",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/vender.html"));
+});
+app.get("/ayuda",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/ayuda.html"));
+});
+app.get("/micompra",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/micompra.html"));
+});
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/registro.html"));
+});
+app.get("/ingresar",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/ingresar.html"));
+});
 
 
 app.use(express.static("public"));

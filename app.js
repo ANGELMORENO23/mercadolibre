9fcd3f2b3cfa564b2b83ba7e6f4bce26e87acdb3
ANGELMORENO23 <chinoman309@hotmail.com>
2023-11-10 20:27:39 -0300

    agrege json de dormularios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const path = require("path")
+const bodyParser = require('body-parser')
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"));
@@ -27,6 +28,13 @@ app.get("/ingresar",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/ingresar.html"));
 });
 
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/registro',(req,res)=> {
+    console.log(req.body);
+    res.redirect("/")
+})
 
 app.use(express.static("public"));
 
